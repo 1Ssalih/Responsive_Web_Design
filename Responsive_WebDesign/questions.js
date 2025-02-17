12f1981311949ce2fdcd805f049327e181b0cd36
1Ssalih <139970596+1Ssalih@users.noreply.github.com>
2025-02-18 00:37:37 +0300

    Add files via upload

---
+++ Responsive_WebDesign/questions.js
@@ -0,0 +1,25 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does Html stand for?",
+        answer: "C. HyperText Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup LAnguage",
+            "D. Home Text Multi Language"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "What does Html stand for?",
+        answer: "C. HyperText Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup LAnguage",
+            "D. Home Text Multi Language"
+        ]
+    }
+];
